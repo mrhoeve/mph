2f745645378830bbd5b9691f8d9d2ab0cb2becd2
Marc Hoeve <mrhoeve@gmail.com>
2026-07-20 11:46:22 +0200

    style(intellij): use the MPH icon throughout the plugin
    - reuse the branded icon for the tool window and context action
    - use the same icon in plugin dialogs and headers
    - retain the dark-theme icon variant
    - remove redundant tool window and action icons

--- intellij-plugin/src/main/kotlin/nl/hicts/mph/intellij/icons/MphIcons.kt
+++ intellij-plugin/src/main/kotlin/nl/hicts/mph/intellij/icons/MphIcons.kt
@@ -4,8 +4,5 @@ import com.intellij.openapi.util.IconLoader
 
 object MphIcons {
     @JvmField
-    val Plugin = IconLoader.getIcon("/META-INF/pluginIcon.svg", MphIcons::class.java)
-
-    @JvmField
-    val UpdateDependents = IconLoader.getIcon("/icons/updateDependents.svg", MphIcons::class.java)
+    val Mph = IconLoader.getIcon("/META-INF/pluginIcon.svg", MphIcons::class.java)
 }
